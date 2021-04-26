fc0b16e9073da85ab11190631c1021a37ee1ce92
Wang Xiao <wx19930910@gmail.com>
2021-04-26 00:13:20 -0400

    add ignore log

--- kernel/kernel-impl/src/test/java/org/sakaiproject/user/impl/test/ContextualDisplayServiceTest.java
+++ kernel/kernel-impl/src/test/java/org/sakaiproject/user/impl/test/ContextualDisplayServiceTest.java
@@ -18,13 +18,10 @@ package org.sakaiproject.user.impl.test;
 
 import java.util.Collection;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import org.sakaiproject.authz.api.AuthzGroupService;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
@@ -40,9 +37,11 @@ import org.sakaiproject.user.api.UserEdit;
 import org.sakaiproject.user.api.UserNotDefinedException;
 import org.sakaiproject.user.impl.DbUserService;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
- * Test that the User Directory Service is correctly enabling context-specific display IDs
- * and display names.
+ * Test that the User Directory Service is correctly enabling context-specific
+ * display IDs and display names.
  */
 @Slf4j
 public class ContextualDisplayServiceTest extends SakaiKernelTestBase {
@@ -58,10 +57,10 @@ public class ContextualDisplayServiceTest extends SakaiKernelTestBase {
 	private UserDirectoryService userDirectoryService;
 	private SiteService siteService;
 	private AuthzGroupService authzGroupService;
-	
+
 	// Declared static since we won't be able to find it as a Spring bean.
 	private static ContextualUserDisplayService contextualUserDisplayService;
-	
+
 	private static String standardSiteUid;
 	private static String contextualSiteUid;
 	private static String currentSiteUid;
@@ -69,71 +68,72 @@ public class ContextualDisplayServiceTest extends SakaiKernelTestBase {
 	@BeforeClass
 	public static void beforeClass() {
 		try {
-            log.debug("starting oneTimeSetup");
+//            log.debug("starting oneTimeSetup");
 			oneTimeSetup();
 			oneTimeSetupAfter();
-            log.debug("finished oneTimeSetup");
+//            log.debug("finished oneTimeSetup");
 		} catch (Exception e) {
-			log.warn(e.getMessage(), e);
+//			log.warn(e.getMessage(), e);
 		}
 	}
-	
+
 	// A full test requires both a display-advising User Directory Provider and a
 	// contextual display provider. An inter-component integration test would manage
 	// via normal Sakai configuration files. For a kernel-internal test, it needs
-	// to be handled less realistically within a single classloader and by explicit injection
+	// to be handled less realistically within a single classloader and by explicit
+	// injection
 	// through Java code.
-	
+
 	// TODO Make it easier to handle test-specific providers in the kernel.
 	private static void oneTimeSetupAfter() throws Exception {
 		TestDisplayAdvisorProvider userDirectoryProvider = new TestDisplayAdvisorProvider();
 		contextualUserDisplayService = new TestContextualUserDisplayService();
-		
-		DbUserService dbUserService = (DbUserService)getService(UserDirectoryService.class);
+
+		DbUserService dbUserService = (DbUserService) getService(UserDirectoryService.class);
 		dbUserService.setProvider(userDirectoryProvider);
 		dbUserService.setContextualUserDisplayService(contextualUserDisplayService);
 	}
-	
+
 	@Before
 	public void setUp() throws Exception {
 		userDirectoryService = getService(UserDirectoryService.class);
 		siteService = getService(SiteService.class);
 		authzGroupService = getService(AuthzGroupService.class);
-		
+
 		actAsUserEid("admin");
-		
+
 		// Add test users.
 		addUserWithEid(NOT_IN_SITE_USER_EID);
 		addUserWithEid(UNADVISED_USER_EID);
 		addUserWithEid(DISPLAY_ADVISED_USER_EID);
-		
+
 		// Add test sites.
 		Site site = siteService.addSite(STANDARD_SITE_NAME, "project");
 		siteService.save(site);
 		standardSiteUid = site.getReference();
-		
+
 		site = siteService.addSite(CONTEXTUAL_SITE_NAME, "project");
 		siteService.save(site);
 		contextualSiteUid = site.getReference();
 	}
-	
+
 	@Test
 	public void testContextualDisplayProvision() throws Exception {
 		User unadvisedUser = userDirectoryService.getUserByEid(UNADVISED_USER_EID);
 		User displayAdvisedUser = userDirectoryService.getUserByEid(DISPLAY_ADVISED_USER_EID);
 		User notInSiteUser = userDirectoryService.getUserByEid(NOT_IN_SITE_USER_EID);
-		
+
 		// Test for default handling of display ID and name outside a site context.
 		checkForUnadvisedProperties(unadvisedUser);
-		
+
 		// Test that the user directory provider handles display ID and name outside
 		// a site context.
 		checkForAdvisedProperties(displayAdvisedUser);
-		
+
 		// This should give the same result in or out of the site context,
 		// but check, just in case....
 		checkForAdvisedProperties(notInSiteUser);
-		
+
 		// Test explicit calls to the test support code....
 		checkExplicitContextualService(unadvisedUser);
 		checkExplicitContextualService(displayAdvisedUser);
@@ -141,7 +141,7 @@ public class ContextualDisplayServiceTest extends SakaiKernelTestBase {
 		Assert.assertEquals(null, contextualUserDisplayService.getUserDisplayName(notInSiteUser, standardSiteUid));
 		Assert.assertEquals(null, contextualUserDisplayService.getUserDisplayId(notInSiteUser, contextualSiteUid));
 		Assert.assertEquals(null, contextualUserDisplayService.getUserDisplayName(notInSiteUser, contextualSiteUid));
-		
+
 		// Set the current site context to make sure the User Directory Service
 		// does the right thing.
 		currentSiteUid = contextualSiteUid;
@@ -150,33 +150,37 @@ public class ContextualDisplayServiceTest extends SakaiKernelTestBase {
 		checkForAdvisedProperties(notInSiteUser);
 		currentSiteUid = null;
 	}
-	
+
 	private void checkForAdvisedProperties(User user) {
 		Assert.assertEquals(user.getEid() + DISPLAY_ADVISED_SUFFIX, user.getDisplayId());
-		Assert.assertEquals(user.getFirstName() + " " + user.getLastName() + DISPLAY_ADVISED_SUFFIX, user.getDisplayName());		
+		Assert.assertEquals(user.getFirstName() + " " + user.getLastName() + DISPLAY_ADVISED_SUFFIX,
+				user.getDisplayName());
 	}
-	
+
 	private void checkForUnadvisedProperties(User user) {
 		Assert.assertEquals(user.getEid(), user.getDisplayId());
-		Assert.assertEquals(user.getFirstName() + " " + user.getLastName(), user.getDisplayName());		
+		Assert.assertEquals(user.getFirstName() + " " + user.getLastName(), user.getDisplayName());
 	}
-	
+
 	private void checkExplicitContextualService(User user) {
 		Assert.assertEquals(null, contextualUserDisplayService.getUserDisplayId(user, standardSiteUid));
 		Assert.assertEquals(null, contextualUserDisplayService.getUserDisplayName(user, standardSiteUid));
-		Assert.assertEquals(user.getEid() + CONTEXTUAL_SITE_NAME, contextualUserDisplayService.getUserDisplayId(user, contextualSiteUid));
-		Assert.assertEquals(user.getFirstName() + " " + user.getLastName() + CONTEXTUAL_SITE_NAME, 
-				contextualUserDisplayService.getUserDisplayName(user, contextualSiteUid));		
+		Assert.assertEquals(user.getEid() + CONTEXTUAL_SITE_NAME,
+				contextualUserDisplayService.getUserDisplayId(user, contextualSiteUid));
+		Assert.assertEquals(user.getFirstName() + " " + user.getLastName() + CONTEXTUAL_SITE_NAME,
+				contextualUserDisplayService.getUserDisplayName(user, contextualSiteUid));
 	}
-	
+
 	private void checkForContextualProperties(User user) {
 		Assert.assertEquals(user.getEid() + CONTEXTUAL_SITE_NAME, user.getDisplayId());
-		Assert.assertEquals(user.getFirstName() + " " + user.getLastName() + CONTEXTUAL_SITE_NAME, user.getDisplayName());		
+		Assert.assertEquals(user.getFirstName() + " " + user.getLastName() + CONTEXTUAL_SITE_NAME,
+				user.getDisplayName());
 	}
 
 	private User addUserWithEid(String eid) throws Exception {
-		User user = userDirectoryService.addUser(null, eid, FIRST_NAME, eid, eid + "@somewhere.edu", eid + "pwd", "Guest", null);
-		log.debug("addUser eid=" + eid + ", id=" + user.getId());
+		User user = userDirectoryService.addUser(null, eid, FIRST_NAME, eid, eid + "@somewhere.edu", eid + "pwd",
+				"Guest", null);
+//		log.debug("addUser eid=" + eid + ", id=" + user.getId());
 		return user;
 	}
 
@@ -200,24 +204,29 @@ public class ContextualDisplayServiceTest extends SakaiKernelTestBase {
 			}
 		}
 
-		// This test class only acts as a display name advisor, not as a user record provider. 
+		// This test class only acts as a display name advisor, not as a user record
+		// provider.
 		public boolean authenticateUser(String eid, UserEdit user, String password) {
 			return false;
 		}
+
 		public boolean authenticateWithProviderFirst(String eid) {
 			return false;
 		}
+
 		public boolean findUserByEmail(UserEdit edit, String email) {
 			return false;
 		}
+
 		public boolean getUser(UserEdit user) {
 			return false;
 		}
+
 		@SuppressWarnings("unchecked")
 		public void getUsers(Collection users) {
 		}
 	}
-	
+
 	public static class TestContextualUserDisplayService implements ContextualUserDisplayService {
 		public String getUserDisplayId(User user, String contextReference) {
 			if (contextualSiteUid.equals(contextReference)) {
@@ -244,25 +253,26 @@ public class ContextualDisplayServiceTest extends SakaiKernelTestBase {
 		public String getUserDisplayName(User user) {
 			return getUserDisplayName(user, currentSiteUid);
 		}
-		
+
 	}
 
 	/**
-	 * Convenience method to switch authn/authz identities.
-	 * TODO Move this frequently-needed helper logic into the base class.
+	 * Convenience method to switch authn/authz identities. TODO Move this
+	 * frequently-needed helper logic into the base class.
 	 *
 	 * @param userEid
 	 */
 	public void actAsUserEid(String userEid) {
-		if (log.isDebugEnabled()) log.debug("actAsUserEid=" + userEid);
-		
+		if (log.isDebugEnabled())
+			log.debug("actAsUserEid=" + userEid);
+
 		SessionManager sessionManager = getService(SessionManager.class);
-		
+
 		String userId;
 		try {
 			userId = userDirectoryService.getUserId(userEid);
 		} catch (UserNotDefinedException e) {
-			log.error("Could not act as user EID=" + userEid, e);
+//			log.error("Could not act as user EID=" + userEid, e);
 			return;
 		}
 		Session session = sessionManager.getCurrentSession();
